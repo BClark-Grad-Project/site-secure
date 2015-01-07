ab970a411c2891a8c80f9d6897788867a6fae775
Brandon L. Clark <support@themindspot.com>
2015-01-07 02:32:01 -0800

    Init package 0.1.0 for testing.

---
+++ index.js
@@ -0,0 +1,59 @@
+/**
+ * Create and bind secured sessions.
+ * Author : Brandon Laurence Clark
+ * License: MIT
+ */
+
+var fs = require('fs');
+var uuid = require('node-uuid');
+var session = require('express-session');
+
+module.exports.create = function(){
+	// Session create
+	var hour = 3600000;
+	
+	return session({ 
+		genid: function() {
+			  return uuid.v4();
+		},
+		secret: 'secret key',
+		cookie: { secure:  true,
+				  expires: new Date(Date.now() + hour),
+				  maxAge:  hour 
+		},
+		saveUninitialized: true,
+	    resave: true
+	});
+};
+
+module.exports.register = function(req, res, next){
+	if(!req.session.user){
+		var sess = req.session;
+		sess.user = {credential: {type:  'guest'}, 
+				     detail:     {first: 'Guest', 
+				    	          last:  'User'}
+			         };
+	}
+	return next();
+};
+	
+module.exports.options = function(){
+	return {
+		key:  fs.readFileSync('./ssl-tls/key.pem'),
+		cert: fs.readFileSync('./ssl-tls/certificate.pem'),
+		requestCert: true
+	};
+}; 
+
+module.exports.port = function(){
+	return 'secure', process.env.PORT || 3443;
+};
+
+// Destroy registered user session.
+module.exports.destroy = function(sess, cb){
+	sess.destroy(function(err){
+		if(err){return cb(err, null);}
+		
+		return cb(null, 'Session Destroyed');
+	});
+};
